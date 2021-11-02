fd03fc151b1986f8290714cea0529c2aef45755f
JM Astrero <201811237@feualabang.edu.ph>
2021-11-02 20:13:28 +0800

    home

--- home.py
+++ home.py
@@ -71,10 +71,10 @@ else:
 
 
 
-hide_streamlit_style = """
-            <style>
+#hide_streamlit_style = """
+            #<style>
             #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+            #footer {visibility: hidden;}
+            #</style>
+            #"""
+#st.markdown(hide_streamlit_style, unsafe_allow_html=True)
